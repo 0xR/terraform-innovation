6db137a1358701dbb27ae83385b5b7f9276f4e62
Ruben Oostinga <roostinga@xebia.com>
2021-04-23 15:00:42 +0200

    Read gcloud credentials from terraform script

--- terraform.sh
+++ terraform.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export GOOGLE_APPLICATION_CREDENTIALS=$(grep credentials_file environments/rjs.tfvars | awk '{print $3}' | tr -d '"')
+
 params=()
 if [ "$1" == init ]; then
   params+=(-backend-config "backend-config/$(whoami).hcl")
